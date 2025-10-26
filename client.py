255e8c1774d881aa890113ad4e3014cc436564ae
ryo-666 <r.otias.0113@gmail.com>
2025-10-27 00:00:20 +0900

    header情報周り

--- client.py
+++ client.py
@@ -26,8 +26,20 @@ import requests
 # print(res.text)
 # print(res.url)
 
-res = requests.get("http://127.0.0.1:8000/items/?skip=1&limit=2")
+# res = requests.get("http://127.0.0.1:8000/items/?skip=1&limit=2")
+
+# print(res.status_code)
+# print(res.text)
+# print(res.url)
+
+# res = requests.post("http://127.0.0.1:8000/items/", json={"name": "Tシャツ", "price": 2000, "description": "白Tシャツ"})
+
+# print(res.status_code)
+# print(res.text)
+# print(res.url)
+
+res = requests.get("http://127.0.0.1:8000/sample/", headers={"Authorization": "Bearer A1B2C3D4"})
 
 print(res.status_code)
 print(res.text)
-print(res.url)
+print(res.headers)
--- main.py
+++ main.py
@@ -1,25 +1,26 @@
-from fastapi import FastAPI, Query
-from typing import Annotated
+from fastapi import FastAPI, Query, Header, Response
+from typing import Annotated, Union
+from pydantic import BaseModel
 
 app = FastAPI()
-items = ["Tシャツ", "スカート", "ブーツ", "コート"]
+# items = ["Tシャツ", "スカート", "ブーツ", "コート"]
 
 # @app.get("/") 
-@app.get("/sample") 
-def read_root():
-  return {"message": "APIです"}
+# @app.get("/sample") 
+# def read_root():
+#   return {"message": "APIです"}
 
-@app.get("/items/sample") 
-def read_item():
-  return {"message": "APIです"}
+# @app.get("/items/sample") 
+# def read_item():
+#   return {"message": "APIです"}
 
-@app.get("/items/{item_id}") 
-def read_item(item_id):
-  return {"item_id": item_id, "item_name": "Tシャツ"}
+# @app.get("/items/{item_id}") 
+# def read_item(item_id):
+#   return {"item_id": item_id, "item_name": "Tシャツ"}
 
-@app.get("/items/{item_id}/detail") 
-def read_item(item_id):
-  return {"item_id": item_id, "item_name": "Tシャツ"}
+# @app.get("/items/{item_id}/detail") 
+# def read_item(item_id):
+#   return {"item_id": item_id, "item_name": "Tシャツ"}
 
 
 """
@@ -29,7 +30,23 @@ gt -> より大きい(指定した値を含まない)
 lt -> より小さい(指定した値を含まない)
 """
 
-@app.get("/items") 
-def read_item(skip: int = 0, limit: Annotated[int, Query(ge=1, le=10)] = 10):
-  return {"items": items[skip: skip + limit]}
+# @app.get("/items") 
+# def read_item(skip: int = 0, limit: Annotated[int, Query(ge=1, le=10)] = 10):
+#   return {"items": items[skip: skip + limit]}
 
+
+# class Item(BaseModel):
+#   name: str
+#   price: float
+#   description: Union[str, None] = None
+
+# @app.post("/items/")
+# def create_item(item: Item):
+#   print(f"データを登録します: {item.name}, {item.price}, {item.description}")
+#   return item
+  
+@app.get("/sample/")
+def read_sample(response: Response ,authorization: Union[str, None] = Header(default=None)):
+  print(authorization)
+  response.headers["custom-header"] = "12345"
+  return {"message": "ヘッダー情報を取得しました"}
